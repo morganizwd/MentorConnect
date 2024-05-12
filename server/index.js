31b28701044d9de4d3b1512aef2d3f38eda39383
morganizwd <90864286+morganizwd@users.noreply.github.com>
2024-05-12 18:27:47 +0300

    feat: import models to index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 require('dotenv').config();
+
 const express = require('express')
 const sequelize = require('./db.js')
+const models = require('./models/models.js')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Working" })
+})
 
 const start = async () => {
     try {
